86661be1f0937728976fd2c953fc22dfa369171f
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-06-08 22:52:11 +0530

    adds enhanced logic

--- ds/src/devmvk/strings/LongestCommonPrefix.java
+++ ds/src/devmvk/strings/LongestCommonPrefix.java
@@ -5,17 +5,22 @@ import java.util.Arrays;
 public class LongestCommonPrefix {
 
     public static String longestCommonPrefix(String[] arr, int n){
-        if(n == 0) return "";
+        if(n == 0) return "-1";
         if(n == 1) return arr[0];
 
         Arrays.sort(arr);
 
         int minCharacters = Math.min(arr[0].length(), arr[n-1].length());
 
-        int i = 0;
-        while (i < minCharacters && arr[0].substring(0, i).equals(arr[n - 1].substring(0, i))){
+        int i = 1;
+        while (i <= minCharacters && arr[0].substring(0, i).equals(arr[n - 1].substring(0, i))){
             i++;
         }
-        return arr[0].substring(0, i);
+
+        if(i == 1) {
+            return "-1";
+        }else{
+            return arr[0].substring(0, i-1);
+        }
     }
 }
--- ds/src/devmvk/strings/Main.java
+++ ds/src/devmvk/strings/Main.java
@@ -3,6 +3,6 @@ package devmvk.strings;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print(ReverseStringWords.reverseString("i.am.varun.kumar.madupu"));
+       System.out.print(LongestCommonPrefix.longestCommonPrefix(new String[]{"geeksforgeeks", "geek", "geeks", "gee"}, 4));
     }
 }
